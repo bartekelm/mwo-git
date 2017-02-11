c5858af022a9b385aea63b2d87ffd43c3d2554cc
Bartosz Elmerych <bartekelm@gmail.com>
2017-02-11 09:46:05 +0100

    Add super feature to the program

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,5 +1,6 @@
 public class SomeProgram {
 	public static void main(String[] args) {
 		System.out.println("We are learning how to Git!");
+		System.out.println("We will be Git experts, soon.");
 	}
 }
